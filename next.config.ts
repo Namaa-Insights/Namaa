1569a625556d51d58c52e2a94e8224c563f329fc
FnoizY <fares.bahamdan@gmail.com>
2025-05-05 18:15:42 +0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ✅ This disables ESLint builds during deployment
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
